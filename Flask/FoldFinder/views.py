8f1abb5f8e0c95b7824281a4a777d709c6a1324e
Will Watson <wcwatson92@gmail.com>
2020-06-10 00:27:12 -0400

    ori_not model 1.0, cosmetic changes to web app

--- Flask/FoldFinder/views.py
+++ Flask/FoldFinder/views.py
@@ -1,14 +1,23 @@
-# views script for FoldFinder app
+# /views script for FoldFinder Flask app
+#
+# Last modified: 2020-06-DD
 
-# Imports
+
+# APP WARM-UP; IMPORT LIBRARIES, ESTABLISH DATABASE CONNECTION, SPIN UP MODELS
+
+# Libraries
 from flask import render_template, request
 from FoldFinder import app
+import os
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import h5py
+import numpy as np
 import pandas as pd
+import tensorflow as tf
 import psycopg2
 import PIL
+import random
 
 # Connect to PostgreSQL
 user = 'wwatson'
@@ -18,40 +27,41 @@ db = create_engine('postgres://%s%s/%s'%(user, host, dbname))
 con = None
 con = psycopg2.connect(database=dbname, user=user)
 
-# Unpickle model, spin up for user input
-origami_cnn = [] #TODO: figure out how this works
-# Dumb-ass "model" for MVP
-def bad_model(img_class):
-    probs = {'butterfly':'75%',
-             'crane':'80%',
-             'duck':'60%',
-             'frog':'50%',
-             'star':'90%'}
-    return probs[img_class]
+# Spin up models, prepare for user input
+ori_not_model = tf.keras.models.load_model('../../Classifiers/ori_not/ori_not_model.h5')
+origami_classifier = tf.keras.models.load_model('../../Classifiers/origami_classifier/origami_classifier.h5') #TODO: check file name
 
 
-# Landing page TODO: eventually make this a nice thing with a button
+# APP FUNCTIONALITY
+
+# Landing page
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
 
-# Page to output classification and directions to user TODO: integrate model
-@app.route('/result')
+# Page(s) to output classification and directions to user TODO: integrate models
+@app.route('/result', methods=['GET', 'POST'])
 def output_result():
-    # Pull image from input field
-    '''
-    user_img = request.args.get('user_img')
-    '''
-    # Feed image through model
-    # TODO: integrate a working model
-    # Dummy code for MVP
-    img_class = request.args.get('img_class')
-    img_p = bad_model(img_class)
-    # SELECT filepath for instructions FROM origami_instructions
-    sql_query = "SELECT instruction_path FROM origami_instructions WHERE instruction_class='%s'" %img_class
-    query_results = pd.read_sql_query(sql_query, con)
-    instruction_fp = query_results.iloc[0]['instruction_path']
-    # Fetch image TODO everything
-    return render_template('result.html', img_class=img_class, img_p=img_p)
+    # Pull image from input field, convert to expected input size
+    user_img = request.files['user_img']
+    IMG_SIZE = 64
+    # TODO: check input size, currently ori_not expects 64x64 rgb, origami_classifier expects 128x128 (rgb)
+    user_img_resized = tf.keras.preprocessing.image.load_img(user_img, target_size=(IMG_SIZE, IMG_SIZE))
+    # Check that the image is actually of origami
+    is_origami = ori_not_model.predict(user_img_resized)
+    # If if is origami, proceed to normal results page
+    if is_origami: #TODO: check and adapt for non-boolean output
+        # Get the origami class
+        img_class = origami_classifier.predict(user_img_resized) #TODO: check and adapt for nature of output
+        # Fetch appropriate instructions from database
+        sql_query = "SELECT instruction_path FROM origami_instructions WHERE instruction_class = '%s'"%img_class
+        query_result = pd.read_sql_query(sql_query, con)
+        instruction_index = random.randint(0, len(query_result))
+        instruction_fp = query_result.iloc[instruction_index]['instruction_path'] #TODO: adapt for URL if needed
+        # Serve results page with appropriate instructions
+        return render_template('result.html', user_img=user_img, img_class=img_class, instruction_fp=instruction_fp)
+    # If the image is not origami, proceed to no_result page
+    else:
+        return render_template('no_result.html', user_img=user_img)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# origami
- 
+# FoldFinder
+This repository contains code for the FoldFinder web app,
+which classifies user-uploaded photos of origami and provides instructions so that the user can replicate origami they especially like.
+
+The app is accessible at {url}.
