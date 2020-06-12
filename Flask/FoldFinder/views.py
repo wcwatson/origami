8e58c99566bb8823af53b5c25ddd4e394ca5d51c
Will Watson <wcwatson92@gmail.com>
2020-06-12 01:20:42 -0400

    Week 3 Pre-demo; Flask integrated w models

--- Flask/FoldFinder/views.py
+++ Flask/FoldFinder/views.py
@@ -17,7 +17,7 @@ import h5py
 import numpy as np
 import pandas as pd
 import psycopg2
-import PIL
+from PIL import Image
 import random
 
 # Connect to PostgreSQL
@@ -29,17 +29,20 @@ con = None
 con = psycopg2.connect(database=dbname, user=user)
 
 # Spin up models, prepare for user input
-ori_not_model = tf.keras.models.load_model('../../Classifiers/ori_not/ori_not_model.h5')
-origami_classifier = tf.keras.models.load_model('../../Classifiers/origami_classifier/origami_classifier.h5')
-origami_classnames = pickle.load(open('../../Classifiers/origami_classifier/origami_classnames.p', 'rb'))
+TEMP_IMG_FP = './FoldFinder/static/user_img.jpg'
+ori_not_model = tf.keras.models.load_model('../Classifiers/ori_not/ori_not_model.h5')
+origami_classifier = tf.keras.models.load_model('../Classifiers/origami_classifier/origami_classifier.h5')
+origami_classnames = pickle.load(open('../Classifiers/origami_classifier/origami_classnames.p', 'rb'))
 
 
 # APP FUNCTIONALITY
 
 # Landing page
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
+    if os.path.exists(TEMP_IMG_FP):
+        os.remove(TEMP_IMG_FP)
     return render_template('index.html')
 
 
@@ -54,25 +57,27 @@ def ethics():
 def output_result():
     # Pull image from input field, temporarily save to static folder
     user_img = request.files['user_img']
-    user_img.save('static/user_img.jpg')
-    # Convert image to expected input size for modeling
+    user_img.save(TEMP_IMG_FP)
+    # Read in image, convert to expected input size for modeling
     IMG_SIZE = 128
-    # TODO: check input size and saturation adjustment multipliers
-    user_img_resized = tf.keras.preprocessing.image.load_img(user_img, target_size=(IMG_SIZE, IMG_SIZE))
+    user_img = tf.keras.preprocessing.image.load_img(TEMP_IMG_FP, target_size=(IMG_SIZE, IMG_SIZE))
+    user_img = tf.keras.preprocessing.image.img_to_array(user_img)
     # Convert to grayscale, check that the image is actually of origami
-    user_img_gray = tf.repeat(tf.image.rgb_to_grayscale(user_img_resized), repeats=3, axis=2)
-    is_origami = (ori_not_model.predict(user_img_gray) == 1)
+    user_img_gray = tf.expand_dims(tf.repeat(tf.image.rgb_to_grayscale(user_img), repeats=3, axis=2), 0)
+    is_origami = ori_not_model.predict_classes(user_img_gray)[0]
     # If if is origami, proceed to normal results page
-    if is_origami:
-        # Adjust image colors, predict class
-        user_img_saturated = tf.image.adjust_saturation(user_img_resized, saturation_factor=2.5)
-        img_class = origami_classifier.predict(user_img_saturated)
-        img_class = origami_classnames[img_class]
+    if is_origami == 1:
+        # Adjust image colors, predict class TODO: double-check saturation adjustment multipliers
+        user_img_saturated = tf.expand_dims(tf.image.adjust_saturation(user_img, saturation_factor=2.5), 0)
+        img_class = origami_classnames[origami_classifier.predict_classes(user_img_saturated)[0]]
+        ''' NICE IDEA, BUT NO
         # Fetch appropriate instructions from database
         sql_query = "SELECT instruction_path FROM origami_instructions WHERE instruction_class = '%s'"%img_class
         query_result = pd.read_sql_query(sql_query, con)
-        instruction_index = random.randint(0, len(query_result))
+        instruction_index = 0 # TODO: random.randint(0, len(query_result))?
         instruction_fp = query_result.iloc[instruction_index]['instruction_path'] #TODO: adapt for URL if needed
+        '''
+        instruction_fp = '../static/instructions/' + img_class + '/1.jpg'
         # Serve results page with appropriate instructions
         return render_template('result.html', img_class=img_class, instruction_fp=instruction_fp)
     # If the image is not origami, proceed to no_result page
